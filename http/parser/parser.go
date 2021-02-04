2510216a18caa471565dd0b6538560b6655f5d82
lesismal <wuqiongjishu@163.com>
2021-02-04 21:04:39 +0800

    + http/parser, save

---
+++ http/parser/parser.go
@@ -0,0 +1,48 @@
+package parser
+
+import (
+	"net/http"
+
+	"github.com/lesismal/lib/bytes"
+)
+
+const (
+	// 1st line
+	stateMethod int = iota
+	stateURI
+	stateProto
+
+	// many header lines
+	stateHeaders
+
+	// header over, empty line
+	stateBlankLine
+
+	// body: chank or content-length
+	stateBody
+)
+
+// Parser .
+type Parser struct {
+	data    []byte
+	buffer  *bytes.Buffer
+	request *http.Request
+}
+
+// Append .
+func (p *Parser) Append(buf []byte) {
+	p.data = append(p.data, buf...)
+}
+
+// Next .
+func (p *Parser) Next() (*http.Request, bool, error) {
+
+	return nil, false, nil
+}
+
+// New .
+func New() *Parser {
+	return &Parser{
+		buffer: bytes.NewBuffer(),
+	}
+}
