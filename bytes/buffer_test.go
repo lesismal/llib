f841a719e84f2151b14d8a3af0e9b28e709ca3bc
lesismal <40462947+lesismal@users.noreply.github.com>
2021-02-16 20:22:39 +0800

    Update buffer_test.go

--- bytes/buffer_test.go
+++ bytes/buffer_test.go
@@ -8,14 +8,14 @@ func TestBuffer(t *testing.T) {
 	buffer := NewBuffer()
 	buffer.Write([]byte("hel"))
 	buffer.Write([]byte("lo world"))
-	b, err := buffer.ReadN(5)
+	b, err := buffer.Read(5)
 	if err != nil {
 		t.Fatal(err)
 	}
 	if string(b) != "hello" {
 		t.Fatal(string(b))
 	}
-	b, err = buffer.ReadN(1)
+	b, err = buffer.Read(1)
 	if string(b) != " " {
 		t.Fatal(string(b))
 	}
@@ -28,14 +28,14 @@ func TestBuffer(t *testing.T) {
 
 	buffer.Write([]byte("hel"))
 	buffer.Write([]byte("lo world"))
-	b, err = buffer.ReadN(5)
+	b, err = buffer.Read(5)
 	if err != nil {
 		t.Fatal(err)
 	}
 	if string(b) != "hello" {
 		t.Fatal(string(b))
 	}
-	b, err = buffer.ReadN(1)
+	b, err = buffer.Read(1)
 	if string(b) != " " {
 		t.Fatal(string(b))
 	}
@@ -43,13 +43,37 @@ func TestBuffer(t *testing.T) {
 	if string(b) != "world" {
 		t.Fatal(string(b))
 	}
-	
+
 	buffer.Reset()
 
 	buffer.Append([]byte("hello"))
 	buffer.Append([]byte(" world"))
 	if string(buffer.buffers[0]) != "hello world" {
 		t.Fatal(string(buffer.buffers[0]))
+	}
+	b, err = buffer.ReadAll()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if string(b) != "hello world" {
+		t.Fatal(string(b))
+	}
+
+	buffer.Reset()
+
+	str := "hello world"
+	buffer.Push([]byte("hello "))
+	buffer.Push([]byte("world"))
+	for i := 0; i < len(str); i++ {
+		for j := i; j < len(str); j++ {
+			sub, err := buffer.Sub(i, j)
+			if err != nil {
+				t.Fatal(err)
+			}
+			if string(sub) != string([]byte(str)[i:j]) {
+				t.Fatalf("[%v:%v] %v != %v", i, j, string(sub), string([]byte(str)[i:j]))
+			}
+		}
 	}
 	b, err = buffer.ReadAll()
 	if err != nil {
