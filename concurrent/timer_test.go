42f5cf575762f6ad80b2f0c39a2d32226aaa37cd
lesismal <lesismal@gmail.com>
2022-02-25 14:38:26 +0800

    + timer_test

---
+++ concurrent/timer_test.go
@@ -0,0 +1,50 @@
+package concurrent
+
+import (
+	"testing"
+	"time"
+)
+
+func checkTolerance(t *testing.T, t0 time.Time, wait, tolerance time.Duration) {
+	used := time.Since(t0) - wait
+	if used < 0 {
+		used = -used
+	}
+	if used > tolerance {
+		t.Fatalf("used %v, expect %v", used, tolerance)
+	}
+}
+func TestTimer(t *testing.T) {
+	timer := NewTimer(nil)
+	timer.Start()
+	defer timer.Stop()
+
+	wait := 100 * time.Millisecond
+	tolerance := 3 * time.Millisecond
+
+	{
+		t0 := time.Now()
+		<-timer.After(wait)
+		checkTolerance(t, t0, wait, tolerance)
+	}
+
+	{
+		t0 := time.Now()
+		chDone := make(chan struct{}, 1)
+		time.AfterFunc(wait, func() {
+			chDone <- struct{}{}
+		})
+		<-chDone
+		checkTolerance(t, t0, wait, tolerance)
+	}
+
+	{
+		t0 := time.Now()
+		chDone := make(chan struct{}, 1)
+		timer.AtOnce(func() {
+			chDone <- struct{}{}
+		})
+		<-chDone
+		checkTolerance(t, t0, 0, tolerance)
+	}
+}
