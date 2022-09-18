c05c216b5dc348657ed94f647dab103ea2258c38
lesismal <lesismal@gmail.com>
2022-09-18 23:16:57 +0800

    clean

--- concurrent/batch_test.go
+++ concurrent/batch_test.go
@@ -6,21 +6,34 @@ package concurrent
 
 import (
 	"log"
+	"sync"
+	"sync/atomic"
 	"testing"
 	"time"
 )
 
 func TestBatch(t *testing.T) {
+	var wg sync.WaitGroup
+	var count int32
+	var timewait = time.Second / 10
 	batchCall := func() (interface{}, error) {
-		time.Sleep(time.Second)
+		atomic.AddInt32(&count, 1)
+		time.Sleep(timewait)
 		return time.Now().Format("2006/01/02 15:04:05.000"), nil
 	}
 	for i := 0; i < 10; i++ {
+		wg.Add(1)
 		go func(id int) {
+			defer wg.Done()
 			ret, err := Do(3, batchCall)
 			log.Println("Batch().Do():", id, ret, err)
 		}(2)
 	}
+	wg.Wait()
+	if n := atomic.LoadInt32(&count); n != 1 {
+		t.Fatalf("Invalid count: %v", n)
+	}
+
 	func(id int) {
 		ret, err := Do(3, batchCall)
 		log.Println("Batch().Do():", id, ret, err)
@@ -30,5 +43,8 @@ func TestBatch(t *testing.T) {
 		ret, err := Do(3, batchCall)
 		log.Println("Batch().Do():", id, ret, err)
 	}(3)
-	time.Sleep(time.Second)
+
+	if n := atomic.LoadInt32(&count); n != 3 {
+		t.Fatalf("Invalid count: %v", n)
+	}
 }
--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/lesismal/llib
 go 1.16
 
 require (
-	github.com/cespare/xxhash v1.1.0 // indirect
+	github.com/cespare/xxhash v1.1.0
 	golang.org/x/crypto v0.0.0-20210513122933-cd7d49e622d5
 	golang.org/x/net v0.0.0-20210510120150-4163338589ed
 )
